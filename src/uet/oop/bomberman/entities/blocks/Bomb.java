f3b5bf205c227ad2d136634e5f96fc0e5cc76e6f
Daohuy2409 <21020329@vnu.edu.vn>
2022-10-29 21:34:38 +0700

    update bomb

--- src/uet/oop/bomberman/entities/blocks/Bomb.java
+++ src/uet/oop/bomberman/entities/blocks/Bomb.java
@@ -2,11 +2,17 @@ package uet.oop.bomberman.entities.blocks;
 
 import javafx.scene.image.Image;
 import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.graphics.Sprite;
+
+import static uet.oop.bomberman.BombermanGame.*;
 
 
 public class Bomb extends Entity {
 
-
+    private static long timeBomb;
+    private static long timeTmp;
+    private static Bomb bomb;
+    private static int swapActive = 1;
     public static int powerBomb = 0;
 
     public static int isBomb = 0;   //0 no bomb /1 had bomb /2 explosion
@@ -15,9 +21,63 @@ public class Bomb extends Entity {
         super(x, y, img);
     }
 
+    public static void putBomb() {
+        if (isBomb == 0) {
+
+            timeBomb = System.currentTimeMillis();
+            timeTmp = timeBomb;
+
+            int x = bomberman.getX() / 32;
+            int y = bomberman.getY() / 32;
+            x = Math.round(x);
+            y = Math.round(y);
+            bomb = new Bomb(x, y, Sprite.bomb.getFxImage());
+            block.add(bomb);
+            isBomb = 1;
+            }
+    }
+    public static void activeBomb() {
+        if (swapActive == 1) {
+            bomb.setImg(Sprite.bomb.getFxImage());
+            swapActive = 2;
+        } else if (swapActive == 2) {
+            bomb.setImg(Sprite.bomb_1.getFxImage());
+            swapActive = 3;
+        } else if (swapActive == 3) {
+            bomb.setImg(Sprite.bomb_2.getFxImage());
+            swapActive = 4;
+        } else {
+            bomb.setImg(Sprite.bomb_1.getFxImage());
+            swapActive = 1;
+        }
+    }
+
+    private static void checkActive() {
+        if (isBomb == 1) {
+            if (System.currentTimeMillis() - timeBomb < 2000) {
+                if (System.currentTimeMillis() - timeTmp > 100) {
+                    activeBomb();
+                    timeTmp += 100;
+                }
+            } else {
+                isBomb = 2;
+                timeBomb = System.currentTimeMillis();
+                timeTmp = timeBomb;
+            }
+        }
+    }
+
+    private static void checkExplosion() {
+        if (isBomb == 2) {
+            block.remove(bomb);
+            isBomb = 0;
+        }
+    }
+
     @Override
     public void update() {
-
+        checkActive();
+        checkExplosion();
     }
 
 }
